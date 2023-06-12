9c9b3ee8b79ad9b735f39479e45f45aa09328f2a
Dmitriy Donchenko <dmitriy.donchenko@nascdev.com>
2023-06-12 15:48:12 -0600

    change def function

--- bot.py
+++ bot.py
@@ -85,54 +85,13 @@ def process_user_input(message):
         send_message_in_parts(chat_id, definition, text)
 
 def get_definition(word):
-    url = f"https://www.dictionaryapi.com/api/v3/references/learners/json/{word}?key={MERRIAM_WEBSTER_API_KEY}"
+    url = f"https://dictionaryapi.com/api/v3/references/learners/json/{word}?key=33f93937-0d40-43f0-92cf-791974312d6d"
     response = requests.get(url)
-    data = response.json()
-
-    if not data:
-        return "No definition found."
-
-    result = ""
-
-    for entry in data:
-        if 'fl' in entry:
-            result += f"\n\nPart of Speech: {entry['fl']}\n"
-
-        if 'shortdef' in entry:
-            result += "\nDefinitions:\n"
-            for definition in entry['shortdef']:
-                result += f"- {definition}\n"
-
-        if 'dros' in entry:
-            result += "\nSpelling Suggestions:\n"
-            for suggestion in entry['dros']:
-                result += f"- {suggestion}\n"
-
-        if 'art' in entry and 'artid' in entry['art']:
-            result += f"\nIllustration: {entry['art']['artid']}\n"
-
-        if 'hwi' in entry and 'prs' in entry['hwi']:
-            result += "\nPronunciations:\n"
-            for pr in entry['hwi']['prs']:
-                if 'mw' in pr:
-                    result += f"- {pr['mw']}\n"
-                if 'sound' in pr and 'audio' in pr['sound']:
-                    result += f"Audio: https://media.merriam-webster.com/soundc11/{word[0]}/{pr['sound']['audio']}.wav\n"
-
-        if 'def' in entry:
-            result += "\nUsage Examples:\n"
-            for def_item in entry['def']:
-                if 'sseq' in def_item:
-                    for sseq_item in def_item['sseq']:
-                        for item in sseq_item:
-                            if isinstance(item, list) and len(item) > 1 and 'dt' in item[1]:
-                                for dt_item in item[1]['dt']:
-                                    if isinstance(dt_item, list) and len(dt_item) > 1 and isinstance(dt_item[1], list):
-                                        for vis_item in dt_item[1]:
-                                            if 't' in vis_item:
-                                                result += f"- {vis_item['t']}\n"
-
-    return result
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data
+    else:
+        return None
 
 
 # Function to get translation from English to Russian
