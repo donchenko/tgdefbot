7f22f253caa360c126097847c9d53b96be097945
Dmitriy Donchenko <dmitriy.donchenko@nascdev.com>
2023-06-12 20:18:49 -0600

    Change lib telegram

--- bot.py
+++ bot.py
@@ -1,122 +1,30 @@
-import os
 import requests
 import json
-import random
-import time
-import re
-import telebot
-from telebot import types
-from dotenv import load_dotenv
-from bs4 import BeautifulSoup
-import logging
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-# Load environment variables from .env file
-load_dotenv()
+bot = Bot(token='5878027549:AAFycCgVZmNC9zZLrFGZYp4ZmmvafVgZL6w')
+dp = Dispatcher(bot)
 
-# Telegram bot token
-TOKEN = os.getenv("TOKEN")
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Hello! I'm a bot for learning English. Send me a word and I'll give you its definition.")
 
-# Merriam-Webster API key
-MERRIAM_WEBSTER_API_KEY = os.getenv("MERRIAM_WEBSTER_API_KEY")
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    word = msg.text
+    response = requests.get(f'https://dictionaryapi.com/api/v3/references/learners/json/{word}?key=33f93937-0d40-43f0-92cf-791974312d6d')
+    data = response.json()[0]
 
-# Initialize the Telegram bot
-bot = telebot.TeleBot(TOKEN)
+    word = data['hwi']['hw']
+    fl = data['fl']
+    shortdef = ', '.join(data['shortdef'])
+    ins = ', '.join([i['if'] for i in data['ins']])
+    defs = '\n'.join([d['dt'][0][1] for d in data['def'][0]['sseq'][0]])
 
-# Configure logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    message = f"Word: {word}\nPart of Speech: {fl}\nShort Definitions: {shortdef}\nOther Forms: {ins}\nDetailed Definitions:\n{defs}"
+    await bot.send_message(msg.from_user.id, message)
 
-# Handler for the "/start" command
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Welcome to the English learning bot!")
-
-# Handler for the "/help" command
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    help_message = """
-    Here are the available commands:
-    - /start: Start the bot
-    - /add [word]: Add a word to your local dictionary
-    - /translate [word]: Get the translation of a word to Russian
-    - /remove [word]: Remove a word from your local dictionary
-    - /reminder: Get a random word reminder from your local dictionary
-    - /help: Show this help message
-    """
-    bot.reply_to(message, help_message)
-
-# Handler for processing user input
-@bot.message_handler(func=lambda message: True)
-def process_user_input(message):
-    chat_id = message.chat.id
-    text = message.text.lower()
-
-    # Check if the user wants to add the word to the local dictionary
-    if text.startswith('/add'):
-        word = text.split(' ', 1)[1]
-        local_dictionary[word] = None
-        send_message_in_parts(chat_id, f"The word '{word}' has been added to your local dictionary.")
-    
-    # Check if the user wants to receive a translation
-    elif text.startswith('/translate'):
-        word = text.split(' ', 1)[1]
-        translation = get_translation(word)
-        if translation:
-            send_message_in_parts(chat_id, f"The translation of '{word}' in Russian is '{translation}'.")
-        else:
-            send_message_in_parts(chat_id, f"Translation not found for the word '{word}'.")
-    
-    # Check if the user wants to remove a word from the local dictionary
-    elif text.startswith('/remove'):
-        word = text.split(' ', 1)[1]
-        if word in local_dictionary:
-            del local_dictionary[word]
-            send_message_in_parts(chat_id, f"The word '{word}' has been removed from your local dictionary.")
-        else:
-            send_message_in_parts(chat_id, f"The word '{word}' is not present in your local dictionary.")
-    
-    # Check if the user wants a random word reminder
-    elif text.startswith('/reminder'):
-        random_word = random.choice(list(local_dictionary.keys()))
-        local_dictionary[random_word] = time.time()
-        send_message_in_parts(chat_id, f"Here's a random word from your local dictionary: {random_word}")
-    
-    # Check if the user wants the definition of a word
-    else:
-        definition = get_definition(text)
-        send_message_in_parts(chat_id, definition, text)
-
-def get_definition(word):
-    url = f"https://dictionaryapi.com/api/v3/references/learners/json/{word}?key=33f93937-0d40-43f0-92cf-791974312d6d"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return data
-    else:
-        return None
-
-
-# Function to get translation from English to Russian
-def get_translation(word):
-    # Implement translation logic here or use an existing translation API
-    return None
-
-# Function to send a message in parts to handle long messages
-def send_message_in_parts(chat_id, text, word,  max_length=4096):
-
-# Add the YouGlish link to the text
-    text += f"\n\nYou can listen to the pronunciation of the word here: https://youglish.com/pronounce/{word}/english"
-   
-    if len(text) <= max_length:
-        bot.send_message(chat_id, text)
-    else:
-        parts = [text[i:i + max_length] for i in range(0, len(text), max_length)]
-        for part in parts:
-            bot.send_message(chat_id, part)
-
-# Start the bot
-if __name__ == "__main__":
-    # Local dictionary to store user-added words
-    local_dictionary = {}
-    
-    logging.info("Starting the bot...")
-    bot.polling()
+if __name__ == '__main__':
+    executor.start_polling(dp)
