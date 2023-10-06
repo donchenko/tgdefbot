62d36c9dbdadbb913aefac314405b3d8f1919656
Dmitriy Donchenko <dmitriy.donchenko@nascdev.com>
2023-10-06 15:51:39 -0600

    Get_definition to module

--- utilities.py
+++ utilities.py
@@ -11,6 +11,53 @@ load_dotenv()
 # Merriam-Webster API key
 MERRIAM_WEBSTER_API_KEY = os.getenv("MERRIAM_WEBSTER_API_KEY")
 
+def get_definition(word):
+    url = f"https://www.dictionaryapi.com/api/v3/references/learners/json/{word}?key={MERRIAM_WEBSTER_API_KEY}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # This will raise an exception if the request failed
+        log_request("definition", word)
+    except requests.exceptions.RequestException as e:
+        log_request("definition", word, success=False, error_message=str(e))
+        return "No definition found due to an error."
+
+    data = response.json()
+    if not data:
+        return "No definition found."
+
+    result = ""
+    for entry in data:
+        # Using get() method to avoid KeyError
+        if entry.get('fl'):
+            result += f"\n\nPart of Speech: {entry['fl']}\n"
+        if entry.get('shortdef'):
+            result += "\nDefinitions:\n"
+            for definition in entry['shortdef']:
+                result += f"- {definition}\n"
+        if entry.get('art') and entry['art'].get('artid'):
+            result += f"\nIllustration: {entry['art']['artid']}\n"
+        if entry.get('hwi') and entry['hwi'].get('prs'):
+            result += "\nPronunciations:\n"
+            for pr in entry['hwi']['prs']:
+                if pr.get('mw'):
+                    result += f"- {pr['mw']}\n"
+                if pr.get('sound') and pr['sound'].get('audio'):
+                    result += f"Audio: https://media.merriam-webster.com/soundc11/{word[0]}/{pr['sound']['audio']}.wav\n"
+        if entry.get('def'):
+            result += "\nUsage Examples:\n"
+            for def_item in entry['def']:
+                if def_item.get('sseq'):
+                    for sseq_item in def_item['sseq']:
+                        for item in sseq_item:
+                            if isinstance(item, list) and len(item) > 1 and item[1].get('dt'):
+                                for dt_item in item[1]['dt']:
+                                    if isinstance(dt_item, list) and len(dt_item) > 1 and isinstance(dt_item[1], list):
+                                        for vis_item in dt_item[1]:
+                                            if isinstance(vis_item, dict) and vis_item.get('t'):
+                                                result += f"- {vis_item['t']}\n"
+    return result
+
+
 
 # Function for formatting text from dictionary to telegram
 def format_text(text):
