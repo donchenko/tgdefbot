fbdf1d93c01e9b0df4cabfc4799e0cd88edb978c
Dmitriy Donchenko <dmitriy.donchenko@nascdev.com>
2023-10-06 16:19:19 -0600

    Connect to DB not finished

--- src/database.py
+++ src/database.py
@@ -1,13 +1,30 @@
 import psycopg2
+from psycopg2 import OperationalError
 
 def connect_to_db():
-    # Your connection logic here
-    pass
+    try:
+        # Replace these values with your database credentials
+        conn = psycopg2.connect(
+            dbname="your_db_name",
+            user="your_db_user",
+            password="your_db_password",
+            host="your_db_host",
+            port="your_db_port"
+        )
+        return conn
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        return None
 
-def add_word_to_db(word, user_id):
+def add_word_to_db(word, definition, user_id):
     conn = connect_to_db()
-    # Logic to add word to database
-    pass
+    cursor = conn.cursor()
+    query = "INSERT INTO user_dictionary (user_id, word, definition) VALUES (%s, %s, %s)"
+    cursor.execute(query, (user_id, word, definition))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 def remove_word_from_db(word, user_id):
     conn = connect_to_db()
