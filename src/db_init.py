dfbedd4c1bd3ae8ca3e2b87663dc2dc32890582c
Dmitriy Donchenko <dmitriy.donchenko@nascdev.com>
2023-10-07 19:29:13 -0600

    debug sql connection

--- src/db_init.py
+++ src/db_init.py
@@ -16,6 +16,9 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
+logging.info(f"DB_HOST: {DB_HOST}, DB_PORT: {DB_PORT}, DB_NAME: {DB_NAME}, DB_USER: {DB_USER}")
+
+
 try:
     conn = psycopg2.connect(
         host=DB_HOST,
