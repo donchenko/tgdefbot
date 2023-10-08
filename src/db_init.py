0e55977c1a8352d443e60a2a5fbdf10177d54974
Dmitriy Donchenko <dmitriy.donchenko@nascdev.com>
2023-10-07 19:15:11 -0600

    add logging

--- src/db_init.py
+++ src/db_init.py
@@ -2,6 +2,11 @@ import psycopg2
 from psycopg2 import sql
 from dotenv import load_dotenv
 import os
+import logging
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+
 
 load_dotenv()
 
@@ -11,13 +16,18 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-conn = psycopg2.connect(
-    host=DB_HOST,
-    port=DB_PORT,
-    dbname=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD
-)
+try:
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    logging.info("Successfully connected to the database.")
+except Exception as e:
+    logging.error(f"Failed to connect to the database: {e}")
+    exit(1)
 
 cursor = conn.cursor()
 
